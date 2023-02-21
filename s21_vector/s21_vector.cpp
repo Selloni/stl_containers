a68d2d65f1c9a61dc4f285486b5591dd2e4518fd
Grandpa Trula <grandpat@ge-l6.kzn.21-school.ru>
2023-02-21 19:37:19 +0300

    std_s21

--- s21_vector/s21_vector.cpp
+++ s21_vector/s21_vector.cpp
@@ -1,3 +1,17 @@
-#include "s21_container.h"
+// #include "s21_container.h"
+#include <vector>
+#include <iostream>
+
+template <typename T>
+void f(T x) {
+  std::cout << "T param" << std::endl;
+}
+
+template <>
+void f<int>(int x) {
+  std::cout << "its int" << std::endl;
+}
 
 
+int main () {
+}
